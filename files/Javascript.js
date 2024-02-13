3db66dce70936726b65760962ab79ed87fcfb1b7
KilledByACake <elisabeth0705@live.no>
2024-02-13 15:51:37 +0100

    lessgooo ting funker

--- files/Javascript.js
+++ files/Javascript.js
@@ -8,10 +8,35 @@ const etternavn = document.getElementById("etternavn").value;
 const telefonnr = document.getElementById("telefonnr").value;
 const epost = document.getElementById("epost").value;
 
-if (!film || !antall || !fornavn || !etternavn || !telefonnr || !epost) {
-alert("Du må fylle ut alle felt for å kjøpe billett.");
-return;
+if (!film) {
+    alert("Du må velge film for å kjøpe billett.");
+    return;
 }
+if (!antall ) {
+    alert("Du må velge antall billetter.");
+    return;
+}
+if (!fornavn) {
+    alert("Du må fylle ut fornavn for å kjøpe billett.");
+    return;
+}
+if (!etternavn) {
+    alert("Du må fylle ut etternavn for å kjøpe billett.");
+    return;
+}
+if (!telefonnr) {
+    alert("Du må fylle ut telefonnummer for å kjøpe billett.");
+    return;
+}
+if (!epost) {
+    alert("Du må fylle ut epost for å kjøpe billett.");
+    return;
+}
+if (!film || !antall || !fornavn || !etternavn || !telefonnr || !epost) {
+    alert("Du må fylle ut alle felt for å kjøpe billett.");
+    return;
+}
+
 
 const billett = {
 film,
